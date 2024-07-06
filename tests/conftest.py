13c262088fd35738157b854b49572c04ce4ba44d
Andrew Buensalida <andrewcbuensalida@gmail.com>
2024-07-06 07:43:59 -0700

    notes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest 
 import source.shapes as shapes
 
-
+# like setup_method in class based testing. Now we can use it as a parameter in test_area in test_rectangle.py
 @pytest.fixture
 def my_rectangle():
     return shapes.Rectangle(length = 10, width = 20)
